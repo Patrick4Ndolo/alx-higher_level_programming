391dc26b495d3663b41c87d0a1f00a9e12089176
Patrick4Ndolo <ndolopatrick52@gmail.com>
2023-01-12 19:57:00 +0000

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    num = 0
+    dent = 0
+
+    for tap in my_list:
+        num += tap[0] * tap[1]
+        dent += tap[1]
+
+    return (num / dent)
---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    list_order = list(a_dictionary.keys())
+    list_order.sort()
+
+    for i in list_ord:
+        print(f"{i}: {a_dictionary.get(i)}")
+
