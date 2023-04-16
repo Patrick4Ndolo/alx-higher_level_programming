38420ede4a733b76fd4bf3a32369fcfdcb438808
Patrick4Ndolo <ndolopatrick52@gmail.com>
2023-04-16 18:00:15 +0300

    python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# This script lists all states from the database hbtn_0e_0_usa
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql username = argv[]
+    mysql password = argv[]
+    db_name = argv[]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT states.id, name FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# This script lists all states with a name starting with N (upper N)
+# from the database hbtn_0e_0_usa
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    username = argv[]
+    password = argv[]
+    db_name = argv[]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT states.id, name FROM states WHERE name "
+                "COLLATE latin1_general_cs "
+                "LIKE 'N%' "
+                "ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
