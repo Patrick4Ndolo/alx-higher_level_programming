828b8ef509ff9786ccce43cb2739ab1402ea7f8a
Patrick4Ndolo <ndolopatrick52@gmail.com>
2023-04-28 15:51:59 +0300

    updated files

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -eq 0 ]; then
-  echo "Please provide a URL as an argument"
-  exit 1
-fi
-
-# Send a DELETE request to the URL and store the response body in a variable
-response_body=$(curl -s -X DELETE $1)
-
-# Display the response body
-echo "$response_body"
+#It Sends a DELETE request to a given URL and display the response body.
+curl -sX DELETE "$1"
